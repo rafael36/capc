cadab93dbc01c4c31b8b397e4849cb9b719b73ac
rafael36 <66216646+rafael36@users.noreply.github.com>
2025-06-14 20:08:56 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 docker run -d --name tailscaled --net=host --privileged tailscale/tailscale tailscaled
 
-docker exec tailscaled tailscale up --auth-key=tskey-auth-kjxTHZ1LJ511CNTRL-QCeFfX2htzRYFA6h4B4hzRGyLgaP2sMN2 --hostname=docker-tailscale
+docker exec tailscaled tailscale up --auth-key=tskey-auth-kHkq3w1QKK11CNTRL-yUuCdJME62gVPjwRES5x2g3iEaUhhDbK --hostname=docker-tailscale
 
 wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/dockerfile
 
