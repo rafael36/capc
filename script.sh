a6c2569ac74fb770c762054452c81c0d888a059f
rafael36 <66216646+rafael36@users.noreply.github.com>
2025-06-14 14:04:30 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+sudo apt install fuse3
+
+sudo usermod -aG fuse $USER
+
 docker run -d --name tailscaled --net=host --privileged tailscale/tailscale tailscaled
 
 docker exec tailscaled tailscale up --authkey=tskey-auth-kt9aVj9SpY11CNTRL-93H6hNrn4NYhUS7amScvNYHPqCzNLCWe --hostname=docker-tailscale
@@ -6,8 +10,10 @@ wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/dockerfile
 
 wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/start.sh
 
+wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/rclone.conf
+
 chmod +x script.sh
 
 docker build -t meu-debian-gui .
 
-docker run -it --net=host meu-debian-gui
+docker run -it --net=host --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined meu-debian-gui
