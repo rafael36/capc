5c6ea0ec5d2aa21e46c3ba2da7d6b8fd67019978
rafael36 <66216646+rafael36@users.noreply.github.com>
2025-06-16 11:05:47 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,3 @@
-
-
-docker run -d --name tailscaled --net=host --privileged tailscale/tailscale tailscaled
-
-docker exec tailscaled tailscale up --auth-key=tskey-auth-kHkq3w1QKK11CNTRL-yUuCdJME62gVPjwRES5x2g3iEaUhhDbK --hostname=docker-tailscale
-
 wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/dockerfile
 
 wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/start.sh
@@ -13,5 +7,3 @@ chmod +x start.sh
 docker build -t meu-debian-gui .
 
 docker run -it --net=host --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined meu-debian-gui
-
-docker ps
