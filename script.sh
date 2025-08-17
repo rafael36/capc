f35089b4f2ad15d9ad2aa08c92f32f86fca944a5
rafael36 <66216646+rafael36@users.noreply.github.com>
2025-08-17 18:35:36 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
+# 1. Rodar o daemon do tailscale (tailscaled) em background
+docker run -d --name tailscaled --net=host --privileged tailscale/tailscale tailscaled
+
+# 2. Conectar o tailscale com sua auth key (vai registrar o container na sua rede)
+docker exec tailscaled tailscale up --authkey=tskey-auth-kt9aVj9SpY11CNTRL-93H6hNrn4NYhUS7amScvNYHPqCzNLCWe --hostname=docker-tailscale
+
+
+
 wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/dockerfile
 
 wget https://raw.githubusercontent.com/rafael36/capc/refs/heads/main/start.sh
